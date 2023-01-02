efa91aba669c2278493163f8f409c88d624fbdae
Sidchou <sidchou93@gmail.com>
2023-01-02 17:16:28 -0500

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance ;
+    public static GameManager Instance
+    {
+        get 
+        {
+            if(_instance == null)
+                _instance = new GameManager();
+            return _instance; 
+        }
+         }
+    [SerializeField]
+    public TargetSpawn targetSpawn;
+    [SerializeField]
+    public UIManager uiManager;
+    private Vector2 _worldPoint;
+    private RaycastHit2D _hit;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+             _worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition+Vector3.forward * 10);
+             _hit = Physics2D.Raycast(_worldPoint, Vector2.zero);
+
+            //If something was hit, the RaycastHit2D.collider will not be null.
+            if (_hit.collider != null)
+            {
+                ITargetable obj = _hit.collider.GetComponent<ITargetable>();
+                if (obj != null)
+                {
+                    obj.OnClick();
+                }
+            }
+        }
+    }
+
+}
+
+
