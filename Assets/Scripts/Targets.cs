0e6e204047c28c169782eeb35ea5e4b5a23d30d6
Sidchou <sidchou93@gmail.com>
2023-01-05 15:44:15 -0500

    Update Targets.cs

--- Assets/Scripts/Targets.cs
+++ Assets/Scripts/Targets.cs
@@ -9,6 +9,15 @@ public class Targets : MonoBehaviour, ITargetable
      public float lifeTime { get; set; } = 5;
     public float spawnTime { get; set; } = 0;
 
+    [SerializeField]
+    private Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private PolygonCollider2D polygonCollider;
+    [SerializeField]
+    List<Vector2[]> colliderBoarder;
+
     public void OnClick()
     {
         GameManager.Instance.targetPool.Deactivate(this.gameObject);
@@ -24,18 +33,42 @@ public class Targets : MonoBehaviour, ITargetable
 
         GameManager.Instance.targetPool.Deactivate(this.gameObject);
     }
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+            Debug.LogError("spriteRenderer is null");
+        GetBoarder();
+    }
 
+    void GetBoarder()
+    {
+        colliderBoarder = new List<Vector2[]>();
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            colliderBoarder.Add(polygonCollider.GetPath(i));
+        }
+        polygonCollider.pathCount = 1;
+    }
+    void SwitchBoarder(int select)
+    {
+        spriteRenderer.sprite = sprites[select];
+        polygonCollider.SetPath(0, colliderBoarder[select]);
+    }
     // Start is called before the first frame update
     void Start()
     {
 
     }
-    
+
+
     private void OnEnable()
     {
-        
+        SwitchBoarder(Random.Range(0,sprites.Length));
+        transform.localScale = Vector3.one * Random.Range(0.8f,1.2f);
+        transform.Rotate(Vector3.forward*Random.Range(0,360));
         spawnTime = Time.time;
-        transform.position = new Vector3(Random.Range(-8.5f, 8.5f), Random.Range(-5f, 5f), 0);
+        transform.position = new Vector3(Random.Range(-8f, 8f), Random.Range(-4.5f, 4.5f), 0);
 
     }
 
@@ -44,8 +77,12 @@ public class Targets : MonoBehaviour, ITargetable
     {
         if (Time.time - spawnTime > lifeTime)
         {
-            OnTimeOut();
+             OnTimeOut();
         }
+
+    }
+    private void OnValidate()
+    {
     }
 
 }
